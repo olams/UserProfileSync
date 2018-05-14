fe61b255a0cdbd6cfe1e599f886ee25f080b8ddf
Ola Marius Sagli <Ola.Marius.Sagli@nrk.no>
2018-05-14 14:13:40 +0200

    Add table view with simple listing

--- UserProfileSync/ViewController.swift
+++ UserProfileSync/ViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //self.tableView.delegate = self
+        //self.tableView.dataSource  = self
+        // self.tableView.reloadData()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 
 }
-
